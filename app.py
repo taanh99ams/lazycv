a7d15e5a25234955bd72963359f65aef49c5db8c
Ta Anh <taanh1999gha@gmail.com>
2018-03-20 11:11:06 +0700

    final project

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/service')
+def service():
+    return render_template('service.html')
+
 if __name__ == '__main__':
   app.run(debug=True)
